7ec4e522ed3e5b5449c2b55b60eede2e75a09440
Beryudha <yudhaanggodho@gmail.com>
2023-09-18 08:37:04 +0700

    modified:   bukti_ngantor/dashboard.php 	modified:   bukti_ngantor/processLogin.php

--- bukti_ngantor/dashboard.php
+++ bukti_ngantor/dashboard.php
@@ -38,7 +38,7 @@ $detail = [
 
     <style>
         .img-bukti-ngantor {
-            width: 100px%;
+            width: 100%;
             aspect-ratio: 1 / 1;
             object-fit: cover:
         }
--- bukti_ngantor/processLogin.php
+++ bukti_ngantor/processLogin.php
@@ -8,37 +8,37 @@ if (isset($_SESSION["user"])) {
 }
 
 // Kalau ada $_POST["mencoba_login"], artinya user sudah mengirimkan data login
-if (isset($_POST["mencoba_login"])) {
-    $username $_POST["username"];
-    $password $_POST["password"];
+if(isset($_POST["mencoba_login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
     // Cek apakah username dan password sudah diisi
     if (empty($username) || empty($password)) {
         // Kalau belum, set error ke session agar bisa ditampilkan di halaman login 
         $_SESSION["error"] = "Username dan password harus diisi!";
-        
+
         // Redirect kembali ke halaman login 
-        header("Location: ./login.php"); 
+        header("Location: ./login.php");
         exit;
     }
 
     // Proses bukti sedang ngantor
     if (!isset($_FILES["bukti_ngantor"])) {
-        $_SESSION["error"] = "Bukti sedang ngantor harus diisi!"; 
-        header("Location: ./login.php"); 
+        $_SESSION["error"] = "Bukti sedang ngantor harus diisi!";
+        header("Location: ./login.php");
         exit;
     }
-    
+
     // Get file dari $_FILES
-    $buktiNgantor = $_FILES["bukti_ngantor"]; 
-    $tmpFile = $buktiNgantor["tmp_name"];
-    
-    $folderTujuan = "./bukti ngantor/";
-    $namaFile = $buktiNgantor["name"]; 
-    $alamatFile = $folderTujuan . $namaFile;
-    
+    $buktiNgantor = $_FILES['bukti_ngantor'];
+    $tmpfile = $buktiNgantor["tmp_name"];
+
+    $folderTujuan = "./bukti_ngantor/";
+    $namaFile = $buktiNgantor["name"];
+    $alamatFile = $folderTujuan . $alamatFile;
+
     // Upload file ke folder tujuan
-    if (!move_uploaded_file($tmpFile, $alamatFile)) {
+    if (!move_uploaded_file($tmpfile, $alamatFile)) {
         $_SESSION["error"] = "Gagal mengupload bukti sedang ngantor!";
         header("Location: ./login.php");
         exit;
@@ -54,3 +54,4 @@ if (isset($_POST["mencoba_login"])) {
     ];
     header("Location: ./dashboard.php");
 }
+
